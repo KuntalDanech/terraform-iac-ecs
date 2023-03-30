2947e873fef3420ec646dd77417cb36c5aa89f1c
Kuntal Danech <Kuntal Danech>
2023-03-30 09:53:27 +0530

    Added provider

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
 provider "aws" {
   region = "us-west-2" # Change to the region you want to create the resources in
 }
